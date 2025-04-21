8e0d50bc6bb40136af74d92f4eb9554c74b878e9
Mael Primet <mael.primet@gmail.com>
2025-04-21 19:39:37 +0200

    feat: update script

--- startapp.sh
+++ startapp.sh
@@ -7,13 +7,5 @@ set -u  # Treat unset variables as an error
 rm -rf /config/userdata/SingletonLock /config/userdata/SingletonCookie
 [ -d "/config/userdata/Default" ] && rm -rf /config/userdata/Default/Singleton*
 
-# Handle custom parameters
-ARGS=""
-
-# Add any custom arguments
-if [ -n "${CHROME_CUSTOM_ARGS}" ]; then
-    ARGS="$ARGS ${CHROME_CUSTOM_ARGS}"
-fi
-
 echo "Starting Chromium browser..."
-exec chromium-browser --user-data-dir=/config/userdata --disk-cache-dir=/config/cache ${CHROMIUM_FLAGS:-} $ARGS
+exec chromium-browser --user-data-dir=/config/userdata --disk-cache-dir=/config/cache --no-first-run --no-sandbox --disable-setuid-sandbox --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --disable-accelerated-2d-canvas --disable-webgl --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --disable-extensions --hide-scrollbars ${CHROME_CUSTOM_ARGS:-}
